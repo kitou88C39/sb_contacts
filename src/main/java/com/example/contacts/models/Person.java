f68443e81f209f8a4e39b71d25d1972cb7abc70e
kitou88C39 <kanfutrooper@icloud.com>
2023-05-26 21:10:13 +0900

    models\Person.javaを編集

--- src/main/java/com/example/contacts/models/Person.java
+++ src/main/java/com/example/contacts/models/Person.java
@@ -1,31 +1,11 @@
 package com.example.contacts.models;
-
+ 
+ import lombok.Getter;
+ import lombok.Setter;
+ @Getter
+ @Setter
+​
 public class Person {
-    private String name;
-    private Integer age;
-    private String email;
-
-    public String getName() {
-        return this.name;
-    }
-
-    public Integer getAge() {
-        return this.age;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-}
+  private String name;
+  private Integer age;
+  private String email;
