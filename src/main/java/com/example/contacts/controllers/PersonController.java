7db16ccd00cd935aafd3e228b46abd7c5e57f860
kitou88C39 <kanfutrooper@icloud.com>
2023-05-27 20:47:22 +0900

    controllers\PersonController.javaを編集

--- src/main/java/com/example/contacts/controllers/PersonController.java
+++ src/main/java/com/example/contacts/controllers/PersonController.java
@@ -1,28 +1,41 @@
 package com.example.contacts.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.contacts.models.Person;
+import com.example.contacts.repository.PersonRepository;
 
 @Controller
 public class PersonController {
 
+    private final PersonRepository repository;
+
+    public PersonController(PersonRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping("/")
-    public String index(@ModelAttribute Person person) {
+    public String index(@ModelAttribute Person person, Model model) {
+        // 一覧用データの用意
+        model.addAttribute("people", repository.findAll());
         return "person/index";
     }
 
     @PostMapping("/create")
-    public String create(@Validated @ModelAttribute Person person, BindingResult result) {
+    public String create(@Validated @ModelAttribute Person person, BindingResult result, Model model) {
 
+        // バリデーションエラーがある場合はindex.htmlを表示
         if (result.hasErrors()) {
+            model.addAttribute("people", repository.findAll());
             return "person/index";
         }
-        return "person/create";
+        repository.saveAndFlush(person);
+        return "redirect:/";
     }
 }
